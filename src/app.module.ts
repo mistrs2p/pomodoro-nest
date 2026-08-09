45f39c29afe7076d9048e2e33e0ef077b5e0f748
Matthew <MahdiMousavi40@Gmail.Com>
2026-08-09 11:48:06 +0330

    feat: add TypeORM and PostgreSQL support with configuration management
    - Added @nestjs/config and @nestjs/typeorm dependencies.
    - Configured TypeORM to connect to a PostgreSQL database using environment variables.
    - Updated app.module.ts to include TypeOrmModule with async configuration.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get<string>('DB_USERNAME'),
+        password: config.get<string>('DB_PASSWORD'),
+        database: config.get<string>('DB_NAME'),
+        // entities: [User, Session, Notification, Post, Comment], // add your entities here
+        synchronize: true, // set to false in production,
+        Inject: [ConfigService],
+      }),
+    }),
+    AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
