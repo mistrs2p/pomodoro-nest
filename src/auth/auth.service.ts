e7bbc17d333a4bf671e635e4c0a65172682d7467
Matthew <MahdiMousavi40@Gmail.Com>
2026-08-09 11:34:22 +0330

    feat: add bcrypt for password hashing in user registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
-  register(createUserDto: CreateUserDto): Promise<CreateUserDto> {
-    return Promise.resolve(createUserDto);
+  async register(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    // encrypt the password before saving the user to the database
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(createUserDto.password, saltRounds);
+    createUserDto.password = hashedPassword;
+    // save user to database logic here
+    return createUserDto;
   }
 }
