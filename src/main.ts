0e90b6753e66f511f06947f2896f16be9c2a7bac
Matthew <MahdiMousavi40@Gmail.Com>
2026-08-11 13:34:11 +0330

    feat: enable CORS with configurable origin and methods in main bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
