6f847714fe3cda665a4cd6bae2c625c446b52654
Mahdi <mahdimousavi40@gmail.com>
2026-08-21 12:28:44 +0100

    feat: enhance JWT strategy to extract token from cookies and validate payload structure

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,14 +6,16 @@ import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
-    async createUser(data: createUserDTO): Promise<User> {
-        const newUser = this.userRepository.create(data);
-        return await this.userRepository.save(newUser);
-    }
+  async createUser(data: createUserDTO): Promise<User> {
+    const newUser = this.userRepository.create(data);
+    return await this.userRepository.save(newUser);
+  }
 
-    async findUserByEmail(email: string): Promise<User | null> {
-        return this.userRepository.findOne({ where: { email } });
-    }
+  async findUserByEmail(email: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { email } });
+  }
 }
