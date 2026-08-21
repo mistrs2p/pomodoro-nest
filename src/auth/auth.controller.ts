d0d2924949de2e48d756f7559d0ff4c3225756ee
Mahdi <mahdimousavi40@gmail.com>
2026-08-21 12:14:31 +0100

    feat: add cookie-based authentication and update CORS origin configuration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseGuards,
+  Res,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { createUserDTO } from './dto/create-user.dto';
 import { LoginDTO } from './dto/login.dto';
 import { UsersService } from 'src/users/users.service';
 import { AuthGuard } from '@nestjs/passport';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -22,10 +31,22 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() LoginDTO: LoginDTO): Promise<{ token: string }> {
+  async login(
+    @Body() LoginDTO: LoginDTO,
+    @Res({ passthrough: true }) res: Response,
+    // ): Promise<{ token: string }> {
+  ): Promise<{ message: string }> {
     // register logic here
     const token = await this.authService.login(LoginDTO);
-    return { token };
+    res.cookie('access_token', token, {
+      httpOnly: true,
+      sameSite: 'strict',
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      path: '/',
+    });
+    return { message: 'Login successful, token set in cookie' };
+    // return { token };
   }
 
   @Get('profile')
